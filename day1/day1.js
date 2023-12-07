1e96a376a53062fce7fbc318d6e744634ed9646a
Jimmy <jimmy-6610@hotmail.com>
2023-12-07 14:36:35 -0500

    day1 part2 in progress

--- day1/day1.js
+++ day1/day1.js
@@ -1,17 +1,39 @@
 const helper = require("../helper.js");
 
 function main() {
+    const textToNumberMap = {
+        one:"1",
+        two:"2",
+        three:"3",
+        four:"4",
+        five:"5",
+        six:"6",
+        seven:"7",
+        eight:"8",
+        nine:"9"
+    };
+
     let data = helper.getData("./day1/info/input.txt");
     let hasNumber = /\d/; 
     let sum = 0; 
 
-    //data = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet", "five8b"]; // Test data
+    // Test data part 1
+    //data = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet", "five8b"]; 
+
+    // Test data part 2
+    data = ["two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen"
+            ]
 
     // Iterating through each string to parse out the left-most and right-most integers to build a number and adding it to the sum
     data.forEach(str => {
-        if (hasNumber.test(str)) {
-            sum += Number(getLeftMostInteger(str) + getRightMostInteger(str));
-        }
+        str = textToNumber(textToNumberMap, str);
+        sum += Number(getLeftMostInteger(str) + getRightMostInteger(str));
     });
 
     console.log(sum);
@@ -39,6 +61,14 @@ function getRightMostInteger(str) {
     }
 
     return null;
+}
+
+function textToNumber(textToNumberMap, str) {
+    str = str.replace(/one|two|three|four|five|six|seven|eight|nine/gi, function(matched){
+        return textToNumberMap[matched];
+    });
+
+    return str;
 }
 
 
