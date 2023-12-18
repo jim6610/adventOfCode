0fbc9aa31bce668afaed3e91c5b9baae1c8e3ba0
Jimmy <jimmy-6610@hotmail.com>
2023-12-18 18:52:38 -0500

    day 5 part 1, code to process data added

--- day5/main.js
+++ day5/main.js
@@ -3,23 +3,103 @@ const helper = require("../helper.js");
 
 function main() {
     let data = helper.getData("./day5/info/input.txt");
-    let sum = 0;
+    let lowestLocation = 0;
 
     /* Test data part 1 and part 2 */
-    // data = [
-    //     "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
-    //     "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
-    //     "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
-    //     "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
-    //     "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
-    //     "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
-    // ];
+    data = [
+        "seeds: 79 14 55 13",
+        "",
+        "seed-to-soil map:",
+        "50 98 2",
+        "52 50 48",
+        "",
+        "soil-to-fertilizer map:",
+        "0 15 37",
+        "37 52 2",
+        "39 0 15",
+        "",
+        "fertilizer-to-water map:",
+        "49 53 8",
+        "0 11 42",
+        "42 0 7",
+        "57 7 4",
+        "",
+        "water-to-light map:",
+        "88 18 7",
+        "18 25 70",
+        "",
+        "light-to-temperature map:",
+        "45 77 23",
+        "81 45 19",
+        "68 64 13",
+        "",
+        "temperature-to-humidity map:",
+        "0 69 1",
+        "1 0 69",
+        "",
+        "humidity-to-location map:",
+        "60 56 37",
+        "56 93 4",
+    ];
 
+    const [seeds, seedToSoil, soilToFertilizer, fertilizerToWater, waterToLight, lightToTemperature, temperatureToHumidity, humidityToLocation] = processAlmanacData(data);
 
-
-    console.log(sum);
+    console.log(soilToFertilizer);
 }
 
+function processAlmanacData(data) {
+    let seeds = [], seedToSoil = new Map(), soilToFertilizer = new Map(), fertilizerToWater = new Map(), waterToLight = new Map(), lightToTemperature = new Map(), temperatureToHumidity = new Map(), humidityToLocation = new Map();
+
+    data.forEach((line, i) => {
+        if (line.includes("seeds:")) {
+            line = line.split("seeds: ");
+            seeds = line[1].split(" ");
+        }
+        else if (line.includes("seed-to-soil map:")) {
+            seedToSoil = buildMap(data, i);
+        }
+        else if (line.includes("soil-to-fertilizer map:")) {
+            soilToFertilizer = buildMap(data, i);
+        }
+        else if (line.includes("fertilizer-to-water map:")) {
+            fertilizerToWater = buildMap(data, i);
+        }
+        else if (line.includes("water-to-light map:")) {
+            waterToLight = buildMap(data, i);
+        }
+        else if (line.includes("light-to-temperature map:")) {
+            lightToTemperature = buildMap(data, i);
+        }
+        else if (line.includes("temperature-to-humidity map:")) {
+            temperatureToHumidity = buildMap(data, i);
+        }
+        else if (line.includes("humidity-to-location map:")) {
+            humidityToLocation = buildMap(data, i);
+        }
+    });
+
+    return [seeds, seedToSoil, soilToFertilizer, fertilizerToWater, waterToLight, lightToTemperature, temperatureToHumidity, humidityToLocation];
+}
+
+function buildMap(data, index) {
+    let map = new Map();
+
+    for (let i = index + 1; i < data.length; i++) {
+        if (data[i] !== "") {
+            const dataSet = data[i].split(" ");
+            let source = Number(dataSet[1]);
+            let destination = Number(dataSet[0]);
+            let range = Number(dataSet[2]);
+
+            for (let j = 0; j < range; j++) {
+                map.set(source + j, destination + j);
+            }
+        }
+        else {
+            return map;
+        }
+    }
+}
 
 
 /* Execute code */
