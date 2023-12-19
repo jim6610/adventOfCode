92d6dfe0945cfab8b8b766a966699b397f6dbb2c
Jimmy <jimmy-6610@hotmail.com>
2023-12-19 18:16:18 -0500

    prep day 6

---
+++ day6/main.js
@@ -0,0 +1,16 @@
+const helper = require("../helper.js");
+
+
+function main() {
+    let data = helper.getData("./day6/info/input.txt");
+
+    /* Test data part 1 and part 2 */
+    data = [
+        "Time:      7  15   30",
+        "Distance:  9  40  200"
+    ];
+
+}
+
+/* Execute code */
+main();
